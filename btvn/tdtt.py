48c662cf2044df9fa45281008c2707552497f91f
nguyendd123 <nguyendangdung1122007@gmail.com>
2025-12-10 12:11:17 +0700

    add 3/6 exercises

---
+++ btvn/tdtt.py
@@ -0,0 +1,27 @@
+# # ex 3
+# n = int (input())
+# print (n//2 + 1)
+
+# --------------------------------------
+
+
+# # ex 4
+# n = int (input())
+# print ("YES" if n % 3 == 0 and n//3 > 1 else "NO")
+
+# --------------------------------------
+
+# # ex 5
+
+# n = int (input ())
+# x = 1
+# for i in range (1, n + 1):
+#     x *= i
+
+# cnt = 0
+# while x % 10 == 0:
+#     cnt += 1
+#     x //= 10
+
+# print (cnt)
+
